8a33fda667a5b67ffadb3d403b36e84240e1935d
CrystyanSantos9 <cryssantos9@gmail.com>
2022-06-04 21:39:33 -0300

    facebook auth done

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-const passport =require('passport')
-const LocalStrategy =  require('passport-local').Strategy
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 
 // console.log(Object.getOwnPropertyNames(passport))
 
@@ -10,32 +12,52 @@ const LocalStrategy =  require('passport-local').Strategy
 router.use(passport.initialize())
 router.use(passport.session())
 
-passport.serializeUser((user, done)=>{
+passport.serializeUser((user, done) => {
     done(null, user)
 })
 
-passport.deserializeUser((user, done)=>{
+passport.deserializeUser((user, done) => {
     done(null, user)
 })
 
-passport.use(new LocalStrategy(async (username, password, done)=>{
-    const user = await User.findOne( { username })
-    if(user){
+passport.use(new LocalStrategy(async (username, password, done) => {
+    const user = await User.findOne({ username })
+    if (user) {
         const isValid = await user.checkPassword(password)
-        if(isValid){
+        if (isValid) {
             return done(null, user)
-        }else{
+        } else {
             return done(null, false)
         }
-    }else{
+    } else {
         return done(null, false)
+    }
+}))
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: 'https://grafana.localhost.com/facebook/callback',
+    profileFields: ['id', 'displayName', 'email', 'photos']
+}, async (accessToken, refreshToken, profile, done) => {
+    const userLocalDB = await User.findOne({ facebookId: profile.id })
+    if (!userLocalDB) {
+        const userFacebook = new User({
+            name: profile.displayName,
+            facebookId: profile.id,
+            roles: ['restrito']
+        })
+        await userFacebook.save()
+        done(null, userFacebook)
+    } else {
+        done(null, userLocalDB)
     }
 }))
 
 router.use((req, res, next) => {
     if (req.isAuthenticated()) {
         res.locals.user = req.user
-        if(!req.session.role){
+        if (!req.session.role) {
             req.session.role = req.user.roles[0]
         }
         res.locals.role = req.session.role
@@ -67,7 +89,15 @@ router.get('/logout', (req, res) => {
 router.post('/login', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
-    failureFlash: false 
+    failureFlash: false
 }))
+
+router.get('/facebook', passport.authenticate('facebook'))
+router.get('/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
+    (req, res) => {
+        res.redirect('/')
+    }
+)
+
 
 module.exports = router 
